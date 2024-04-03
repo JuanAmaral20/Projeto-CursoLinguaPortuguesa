4b5963e84bc52a8290b359fc7d57a23894aa1956
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-04-03 15:40:28 -0300

    Adicionado trigger de parcelas

--- triggers.sql
+++ triggers.sql
@@ -15,3 +15,36 @@ CREATE OR ALTER TRIGGER TRG_InserirNomeCompleto
                 WHERE Id = @IdI
         END
     GO
+
+CREATE OR ALTER TRIGGER [dbo].[inserirParcelas]
+	ON Pagamento
+	AFTER INSERT
+
+	AS
+	BEGIN
+		-- variaveis
+		DECLARE @pagamentoIdI INT,
+				@quantidadeI INT,
+				@valorTotal DECIMAL(15, 2),
+				@contador INT = 0
+
+		-- inserindo valores
+		SELECT	@pagamentoIdI = Id,
+				@quantidadeI = quantidadeParcelas,
+				@valorTotal  = ValorTotal FROM inserted
+		
+
+	
+		IF (@pagamentoIdI IS NOT NULL) AND (@quantidadeI IS NOT NULL)
+			
+			WHILE @contador != @quantidadeI
+				BEGIN
+					SET @contador += 1
+
+					INSERT INTO Parcela (IdPagamento, ValorParcela, DataVencimento, Pago)
+						VALUES (@pagamentoIdI, @valorTotal / @quantidadeI, DATEADD(MONTH, @contador, GETDATE()), 0)
+				END
+
+			
+	END
+GO
