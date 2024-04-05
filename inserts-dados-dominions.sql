3c248368f9ca798d4fb657f0ef2c00a7571c88a9
Juan Amaral <pabloah09@gmail.com>
2024-04-05 09:16:04 -0300

    Enviando mais alguns inserts na tabela de domínio

--- inserts-dados-dominions.sql
+++ inserts-dados-dominions.sql
@@ -3,3 +3,11 @@ INSERT INTO Usuario (Nome, Senha, Email, CPF, Sobrenome) VALUES ('JURANDIR', '12
 INSERT INTO Usuario (Nome, Senha, Email, CPF, Sobrenome, IdProfessor) VALUES ('JURANDIR', '123456789', 'JURANDIRPROFESSOR@GMAIL.COM', 13245678908, 'TECIDOS', 1)
 
 INSERT INTO TipoCurso (Nome) VALUES ('SISTEMAS INFORMAÇÃO')
+
+INSERT INTO Dificuldade(Nivel) VALUES('Fácil'), ('Intermediário'), ('Avançado')
+
+INSERT INTO TipoCurso (Nome) VALUES ('Graduação'), ('Pós-Graduação'), ('Extensão'), ('Certificação')
+
+INSERT INTO TipoPagamento (Nome) VALUES ('Cartão de Crédito'), ('Pix')
+
+INSERT INTO UF (NomeUF, Sigla) VALUES ('Rio de Janeiro', 'RJ'), ('São Paulo', 'SP'), ('Minas Gerais', 'MG'), ('Bahia', 'BA'), ('Paraná', 'PR')
