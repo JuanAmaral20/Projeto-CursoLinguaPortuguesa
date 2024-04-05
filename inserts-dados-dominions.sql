bc9889e4948932b74cfd0eee853bca0e944907f7
Juan Amaral <pabloah09@gmail.com>
2024-04-05 09:21:45 -0300

    reajustando  alguns inserts na tabela de domínio

--- inserts-dados-dominions.sql
+++ inserts-dados-dominions.sql
@@ -1,9 +1,3 @@
-INSERT INTO Usuario (Nome, Senha, Email, CPF, Sobrenome) VALUES ('JURANDIR', '123456789', 'JURANDIR@GMAIL.COM', 13245678909, 'TECIDOS')
-
-INSERT INTO Usuario (Nome, Senha, Email, CPF, Sobrenome, IdProfessor) VALUES ('JURANDIR', '123456789', 'JURANDIRPROFESSOR@GMAIL.COM', 13245678908, 'TECIDOS', 1)
-
-INSERT INTO TipoCurso (Nome) VALUES ('SISTEMAS INFORMAÇÃO')
-
 INSERT INTO Dificuldade(Nivel) VALUES('Fácil'), ('Intermediário'), ('Avançado')
 
 INSERT INTO TipoCurso (Nome) VALUES ('Graduação'), ('Pós-Graduação'), ('Extensão'), ('Certificação')
